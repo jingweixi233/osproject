feb0679af04c206285aa783b433fe360b08f5a18
jingweixi <jingweixi@sjtu.edu.cn>
2019-04-13 11:16:11 +0800

    finish pro3

---
+++ problem3/jni/process.c
@@ -0,0 +1,18 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+int main(){
+	pid_t pid;
+	pid = fork();
+	if(pid == 0){
+		printf("Child pid = %d\n", getpid());
+		execl("/data/misc/ospro/ptreeARM", "ptreeARM", NULL);
+	}
+	else{
+		printf("Parent pid = %d\n", getpid());
+	}
+	return 0;
+}
